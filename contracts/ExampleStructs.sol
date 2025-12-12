f61cba2b764cca895f5d91f2cc841f107d5dac4f
harrison70707 <prc.harrisongough@gmail.com>
2025-12-12 11:08:14 -0600

    section 61

--- contracts/ExampleStructs.sol
+++ contracts/ExampleStructs.sol
@@ -2,6 +2,32 @@
  
 pragma solidity 0.8.14;
 
-contract StructsEX {
-    
+contract Wallet {
+    paymentReceived public payment;
+
+    function payContract() public payable {
+        payment = new paymentReceived(msg.sender, msg.value);
+    }
+}
+
+contract paymentReceived {
+    address public from;
+    uint public amount;
+
+    constructor(address _from, uint _amount){
+        from = _from;
+        amount = _amount;
+    }
+}
+
+contract Wallet2 {
+    struct PaymentReceivedStruct{
+        address from;
+        uint amount;
+    }
+    PaymentReceivedStruct public payment;
+
+    function payContract() public payable {
+        payment = PaymentReceivedStruct(msg.sender, msg.value);
+    }
 }
