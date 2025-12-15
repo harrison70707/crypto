90e5e0dd126287042f825efec23a8f6e690a8910
harrison70707 <prc.harrisongough@gmail.com>
2025-12-15 11:25:24 -0600

    structs example

---
+++ contracts/mappingStructs.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: MIT
+ 
+pragma solidity 0.8.15;
+
+contract MappingEx{
+    struct Transaction{
+        uint amount;
+        uint timestamp;
+    }
+
+    struct Balance {
+        uint totalBalance;
+        uint numDeposits;
+        mapping(uint => Transaction) deposits;
+        uint numWithdrawls;
+        mapping(uint => Transaction) withdrawls;
+    }
+
+
+
+    mapping(address => Balance) public balances;
+
+    function getDepositsNum(address _from, uint _numDeposit) public view returns(Transaction memory) {
+        return balances[_from].deposits[_numDeposit];
+    }
+
+    function depositMoney() public payable {
+        balances[msg.sender].totalBalance += msg.value;
+
+        Transaction memory deposit = Transaction(msg.value, block.timestamp);
+        balances[msg.sender].deposits[balances[msg.sender].numDeposits] = deposit;
+        balances[msg.sender].numDeposits++;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        balances[msg.sender].totalBalance -= _amount;
+
+        // create a Transaction and store it using the correct names
+        Transaction memory withdrawal = Transaction(_amount, block.timestamp);
+        balances[msg.sender].withdrawls[balances[msg.sender].numWithdrawls] = withdrawal;
+        balances[msg.sender].numWithdrawls++;
+
+        _to.transfer(_amount);
+
+    }
+
+}
