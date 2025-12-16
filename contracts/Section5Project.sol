456a63528f5ac8ff1d57b63ea469f92bb1967ede
harrison70707 <prc.harrisongough@gmail.com>
2025-12-16 12:49:24 -0600

    start of wallet

---
+++ contracts/Section5Project.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+ 
+pragma solidity 0.8.16;
+
+contract SmartWallet {
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory){
+        require(msg.sender == owner);
+        (bool success, bytes memory returnData) = _to.call{value: _amount}(_payload);
+        require(success);
+        return returnData;
+    }
+
+    receive() external payable { }
+}
